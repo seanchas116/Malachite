49e5aab788f9f891f8369a12d149e61447e84787
iofg2100 <iofg2100@gmail.com>
2013-02-07 14:33:43 +0900

    fixed Interval

--- src/interval.h
+++ src/interval.h
@@ -4,6 +4,7 @@
 //ExportName: Interval
 
 #include <algorithm>
+#include <array>
 
 namespace Malachite {
 
@@ -11,27 +12,49 @@ class Interval
 {
 public:
 	
-	Interval() : _start(0), _end(-1) {}
-	Interval(int start, int length) : _start(start), _end(start + length - 1) {}
+	Interval() : _start(0), _end(0) {}
+	Interval(int start, int length) : _start(start), _end(start + length) {}
 	
 	int start() const { return _start; }
-	int length() const { return _end - _start + 1; }
+	int length() const { return _end - _start; }
 	int end() const { return _end; }
 	
-	bool isValid() const { return _end >= _start; }
+	bool isValid() const { return _end > _start; }
 	
 	void setStart(int start) { _start = start; }
 	void setEnd(int end) { _end = end; }
-	void setLength(int len) { _end = _start + len - 1; }
+	void setLength(int len) { _end = _start + len; }
 	
 	void shift(int diff)
 	{
-		_start += diff;
-		_end += diff;
+		*this = shifted(diff);
+	}
+	
+	Interval shifted(int diff) const
+	{
+		Interval result;
+		result.setStart(start() + diff);
+		result.setEnd(end() + diff);
+		return result;
 	}
 	
 	void moveStart(int start) { shift(start - _start); }
 	void moveEnd(int end) { shift(end - _end); }
+	
+	std::array<Interval, 2> subtracted(const Interval &other) const
+	{
+		return subtract(*this, other);
+	}
+	
+	static std::array<Interval, 2> subtract(const Interval &i1, const Interval &i2)
+	{
+		Interval left, right;
+		left.setStart(i1.start());
+		left.setEnd(std::min(i2.start(), i1.end()));
+		right.setStart(std::max(i2.end(), i1.start()));
+		right.setEnd(i1.end());
+		return {{left, right}};
+	}
 	
 private:
 	
